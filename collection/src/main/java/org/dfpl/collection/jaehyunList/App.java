27f454a66449238c50fd57e3cf80b25c3d4e42d4
JaehyunAhn <jaehyen2@naver.com>
2021-04-19 15:58:06 +0900

    Add jaehyunList

---
+++ collection/src/main/java/org/dfpl/collection/jaehyunList/App.java
@@ -0,0 +1,12 @@
+package org.dfpl.collection.jaehyunList;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JLinkedList list = new JLinkedList<>();
+		
+		
+	}
+
+}
